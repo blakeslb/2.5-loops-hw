b4ed39b05b47675b00190fdfbccc61695a3a4ff2
Bianca Blakesley <bianca.blakesley@gmail.com>
2022-03-11 17:08:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Solve the following problems, you will need to determine which for loop to use, or if one is needed at all:
+
+
+// 1. Create a function that returns the number of syllables in a simple string. The string is made up of short repeated words like "Lalalalalalala" (which would have 7 syllables).
+
+
+// 2. Loop through the `myFavoriteFoods` array and print each element to the console as a numbered list 
+
+let myFavoriteFoods = ["pizza", "chocolate", "waffles", "pasta", "ramen"];
+
+// 3. Create a function which returns "upper" if all the letters in a word are uppercase, "lower" if lowercase and "mixed" for any mix of the two.
+
+// 4. Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
+
+// 5. Write a JavaScript for loop that will iterate from 0 to 25. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+// Your results should look like this:
+// "0 is even"
+// "1 is odd"
+// "2 is even"
+
+// 6. You have a shelf that can only hold so many books. For each iteration of the loop, we will add one book onto the shelf. Once the shelf has 80 books, you can no longer add anymore. Write a script that after every iteration, it will tell you how many remaining books you can add into the shelf. Console long something like, "There is room for only ## of books"
+
+// 7. Print out a grid of asterisks. Ask the user for the height (rows) and width (columns).
+
+
+// 8.  Write a function that accept two integers and displays the larger number.
+
+
+// 9. Write a function that takes in a string and removes all vowels from it (a, e, i, o, u), and returns the new string. If the string is empty, break from the loop.
